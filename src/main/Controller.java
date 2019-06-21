3bedbed26000336525132a319c1d0c2efbd22529
Mastader <b04902022@ntu.edu.tw>
2019-06-21 20:59:47 +0800

    Added userview

--- src/main/Controller.java
+++ src/main/Controller.java
@@ -1,21 +1,72 @@
 package main;
 
 import java.awt.BorderLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Observer;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
+import utils.User;
+import utils.UserView;
 
-public class Controller extends JPanel{
-	Controller() {
-        super(new GridLayout(3, 1));
-        JButton game1Button = new JButton("Game 1");
-        JButton game2Button = new JButton("Game 2");
-        JButton game3Button = new JButton("Game 3");
-        this.add(game1Button);
-        this.add(game2Button);
-        this.add(game3Button);
+
+public class Controller extends JPanel {
+	Buttons thing;
+	
+	private class Buttons extends JPanel {
+		JButton game1Button;
+		JButton game2Button;
+		JButton game3Button;
+		
+		GridBagConstraints config = new GridBagConstraints();
+		
+		Buttons() {
+			setLayout(new GridBagLayout());
+			
+			config.anchor = GridBagConstraints.CENTER;
+			config.gridwidth = GridBagConstraints.REMAINDER;
+			
+	        game1Button = new JButton("Game 1");
+	        game1Button.addActionListener(new ButtonHandler());
+	        
+	        game2Button = new JButton("Game 2");
+	        game2Button.addActionListener(new ButtonHandler());
+	        
+	        game3Button = new JButton("Game 3");
+	        game3Button.addActionListener(new ButtonHandler());
+	        
+	        this.add(game1Button, config);
+	        this.add(game2Button, config);
+	        this.add(game3Button, config);
+		}
+	}
+	
+	private class ButtonHandler implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+        	String command = "You pressed " + e.getActionCommand();
+        	JButton newOne = new JButton(command);
+        	newOne.addActionListener(new ButtonHandler());
+        	thing.add(newOne, thing.config);
+        	thing.repaint();
+        	thing.revalidate();
+        	
+        	thing.remove((JButton) e.getSource());
+        	
+        	thing.repaint();
+        	thing.revalidate();
+        }
+    }
+	
+	Controller(User user) {
+		setLayout(new BorderLayout());
+		thing = new Buttons();
+		this.add(new UserView(user),BorderLayout.NORTH);
+		this.add(thing, BorderLayout.CENTER);
 	}
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -15,32 +15,29 @@ import utils.Login;
 import utils.User;
 
 public class Main implements Observer{
-	static JFrame f;
+	static JFrame window;
 	static boolean loggedIn = false;
 	static Login loginPage;
 	
 	public static void main(String[] args) {
 		Main index = new Main();
-        f = new JFrame();
-        f.setLayout(new BorderLayout());
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window = new JFrame();
+        window.setLayout(new BorderLayout());
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         loginPage = new Login();
         loginPage.addObserver(index);
-       	f.add(loginPage, BorderLayout.CENTER);
-       	f.setExtendedState(f.getExtendedState() | JFrame.MAXIMIZED_BOTH);
-        f.setLocationRelativeTo(null);
-        f.setVisible(true);
+       	window.add(loginPage, BorderLayout.CENTER);
+       	window.setExtendedState(window.getExtendedState() | JFrame.MAXIMIZED_BOTH);
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
 	}
 
 	@Override
 	public void update(Observable o, Object arg) {
 		User temp = (User) arg;
-		JLabel text = new JLabel();
-		text.setText("Welcome, " + temp.name + ". You currently have $" + temp.getMoney() + " left.");
-		f.remove(loginPage);
-		f.add(text, BorderLayout.NORTH);
-		f.add(new Controller(), BorderLayout.CENTER);
-		f.repaint();
-		f.revalidate();
+		window.remove(loginPage);
+		window.add(new Controller(temp), BorderLayout.CENTER);
+		window.repaint();
+		window.revalidate();
 	}
 }
