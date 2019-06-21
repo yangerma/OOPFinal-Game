f6f4ffa71c0ef84c2e9cc73077072e392f21dbf1
Lin, Ting-Wei <t1016d@gmail.com>
2019-06-22 02:32:25 +0800

    Add communication between User & UserView

--- src/utils/User.java
+++ src/utils/User.java
@@ -1,25 +1,34 @@
 package utils;
 
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
 public class User {
-	private String name;
-	private String pw;
-	private int money;
-	
-	public User(String name, String pw) {
-		money = 0;
-		this.name = new String(name);
-		this.pw = new String(pw);
-	}
-	
-	public String getName() {
-		return new String(name);
-	}
+    private String name;
+    private String pw;
+    private int money;
 
-	public int getMoney() {
-		return money;
-	}
+    private final PropertyChangeSupport pcs = new PropertyChangeSupport(this);
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        this.pcs.addPropertyChangeListener(listener);
+    }
 
-	public void setMoney(int money) {
-		this.money = money;
-	}
+    public User(String name, String pw) {
+        money = 0;
+        this.name = new String(name);
+        this.pw = new String(pw);
+    }
+
+    public String getName() {
+        return new String(name);
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public void setMoney(int money) {
+        this.money = money;
+        pcs.firePropertyChange("money", -1, money);
+    }
 }
--- src/utils/UserView.java
+++ src/utils/UserView.java
@@ -1,15 +1,29 @@
 package utils;
 
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class UserView extends JPanel {
-	User user;
-	
-	public UserView(User user) {
-		this.user = user;
-		JLabel text = new JLabel();
-		text.setText("Welcome, " + user.getName() + ". You currently have $" + user.getMoney() + " left.");
-		add(text);
-	}
+public class UserView extends JPanel implements PropertyChangeListener {
+    User user;
+    JLabel text;
+
+    public UserView(User user) {
+        this.user = user;
+        user.addPropertyChangeListener(this);
+        text = new JLabel();
+        text.setText(statusString());
+        add(text);
+    }
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+        text.setText(statusString());
+    }
+
+    private String statusString() {
+        return "Welcome, " + user.getName() + ". You currently have $" + user.getMoney() + " left.";
+    }
 }
