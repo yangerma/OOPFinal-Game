0462bb6eec8744f5b4a30f43b868d78a9ae4d96f
Lin, Ting-Wei <t1016d@gmail.com>
2019-06-21 21:56:49 +0800

    Add PropertyChange* in game/*.java

--- src/game/Model.java
+++ src/game/Model.java
@@ -1,5 +1,14 @@
 package game;
 
-public interface Model {
 
+import java.beans.*;
+
+import javax.swing.*;
+
+public abstract class Model {
+	protected final PropertyChangeSupport pcs = new PropertyChangeSupport(this);
+
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        this.pcs.addPropertyChangeListener(listener);
+    }
 }
--- src/game/View.java
+++ src/game/View.java
@@ -1,5 +1,11 @@
 package game;
 
-public interface View {
+import java.beans.PropertyChangeListener;
 
+import javax.swing.*;
+
+public abstract class View extends JPanel implements PropertyChangeListener {
+	public View(Model model) {
+		model.addPropertyChangeListener(this);
+	}
 }
