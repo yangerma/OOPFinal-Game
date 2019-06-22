672e1fb36ce55cc958972f0054443af1e0ee6b84
Lin, Ting-Wei <t1016d@gmail.com>
2019-06-22 18:43:43 +0800

    Add showMessage to game.View

--- src/game/View.java
+++ src/game/View.java
@@ -8,4 +8,10 @@ public abstract class View extends JPanel implements PropertyChangeListener {
 	public View(Model model) {
 		model.addPropertyChangeListener(this);
 	}
+	protected void showMessage(String message) {
+	    showMessage(message, "Message");
+	}
+	protected void showMessage(String message, String title) {
+	    JOptionPane.showMessageDialog(this, message, title, JOptionPane.WARNING_MESSAGE);
+	}
 }
