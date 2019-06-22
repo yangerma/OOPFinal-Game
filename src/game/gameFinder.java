73d3f0793e2dc55bf107207f4eceabb094c9615d
Yanger Ma <yangerma_2@mayanggede-MacBook-Pro.local>
2019-06-22 22:27:05 +0800

    Use constant strings to prevent typo.

--- src/game/gameFinder.java
+++ src/game/gameFinder.java
@@ -5,15 +5,20 @@ import java.util.*;
 import utils.User;
 
 public class gameFinder {
-    List<String> nameList = new ArrayList<String>();
+	//private final String gameNames = {"Button Growth Game", "Shi Ba La", "Hourse Game"};
+	private final String buttonGrowth = "Button Growth Game";
+	private final String shibala = "Shi Ba La";
+	private final String hourse = "Hourse Game";
+	private final String guessNumber = "Guess Number";
+	List<String> nameList = new ArrayList<String>();
     User user;
     public gameFinder(User user) {
         this.user = user;
         
-        // Button Growth Game
-        nameList.add("Button Growth Game");
-        nameList.add("Shi Ba La");
-        nameList.add("Hourse Game");
+        nameList.add(buttonGrowth);
+        nameList.add(shibala);
+        nameList.add(hourse);
+        nameList.add(guessNumber);
     }
     
     public List<String> gameNames() {
@@ -22,12 +27,14 @@ public class gameFinder {
     
     public View findGame(String name) {
         switch (name) {
-        case "Button Growth Game" :
+        case buttonGrowth :
             return new game.buttonGrowth.View(new game.buttonGrowth.Model(user));
-        case "Shi Ba La" :
+        case shibala:
             return new game.shibala.View(new game.shibala.Model(user));
-        case "Hourse Game":
+        case hourse:
         	return new game.hourse.View(new game.hourse.Model(user));
+        case guessNumber:
+        	return new game.guessNumber.View(new game.guessNumber.Model(user));
         }
         assert(false);
         return null;
